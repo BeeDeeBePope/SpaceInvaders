4c683bdcfacde6cceb192c3c19322f774a02ed78
Adam <204019@edu.p.lodz.pl>
2020-07-05 23:24:39 +0200

    Working on player and enemy classes

--- main.py
+++ main.py
@@ -34,6 +34,42 @@ def initialize():
     pygame.font.init()
 
 
+class Ship:
+    def __init__(self, x, y, image, laser, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.max_health = health
+        self.image = image
+        self.laser = laser
+        self.lasers = []
+        self.cool_down_counter = 0
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
+        # color = (255, 0, 0)
+        # rect = (self.x, self.y, 50, 50)
+        # pygame.draw.rect(window, color, rect)
+
+    def get_width(self):
+        return self.image.get_width()
+
+    def get_height(self):
+        return self.image.get_height()
+
+
+class Player(Ship):
+    def __init__(self, x, y, image, laser, health=100):
+        super().__init__(x, y, image, laser, health=health)
+
+
+class Enemy(Ship):
+    def __init__(self, x, y, image, laser, health=100):
+        super().__init__(x, y, image, laser, health=health)
+
+
 def main():
     """Main function of Space Invaders game
     """
@@ -53,6 +89,7 @@ def main():
     FPS = 60
     lives = 5
     level = 1
+    player_vel = 5
     clock = pygame.time.Clock()
 
     ui_margin = {
@@ -62,6 +99,8 @@ def main():
         "bottom": 10,
     }
 
+    player = Player(300, 300, assets_library.assets.ships.yellow.data, assets_library.assets.projectiles.laser_yellow.data)
+
     def redraw_window():
         window.blit(background_img.get_image(inputs.width_height), (0, 0))
 
@@ -70,6 +109,9 @@ def main():
 
         window.blit(lives_label, (ui_margin["left"], ui_margin["top"]))
         window.blit(level_label, (inputs.width_height[0] - level_label.get_width() - ui_margin["right"], ui_margin["top"]))
+
+        player.draw(window)
+
         pygame.display.update()
 
     while run:
@@ -80,6 +122,23 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] and player.x - player_vel > 0:
+            player.x -= player_vel
+
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < inputs.width_height[0]:
+            player.x += player_vel
+
+        if keys[pygame.K_w] and player.y - player_vel > 0:
+            player.y -= player_vel
+
+        if keys[pygame.K_s] and player.y + player_vel + player.get_width() < inputs.width_height[1]:
+            player.y += player_vel
+
+        if keys[pygame.K_ESCAPE]:
+            run = False
 
     print("Game ended")
 
